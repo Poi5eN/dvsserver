df302b6224b26b9d9beea49fbe00a24a652a738b
Poi5eN <gaurav.upadhyay.vasudeva@gmail.com>
2025-03-23 18:58:41 +0530

    updated reports

--- routes/exam.js
+++ routes/exam.js
@@ -10,6 +10,9 @@ const {
   generateReportCard,
   getExamAnalytics,
   generateFullReportCard,
+  generateClassReport,
+  getPerformanceAnalytics,
+  updateReportCard,
 } = require("../controllers/examController");
 
 router.post('/exams', verifyToken, createExam);
@@ -18,7 +21,10 @@ router.put('/exams/:id', verifyToken, updateExam);
 router.delete('/exams/:id', verifyToken, deleteExam);
 router.post('/exams/:id/results', verifyToken, submitExamResults);
 router.get('/exams/:examId/students/:studentId/report-card', verifyToken, generateReportCard);
-router.get('/exams/:id/analytics', verifyToken, getExamAnalytics);
+router.get('/results/:id/analytics', verifyToken, getExamAnalytics);
 router.get('/results/:studentId/report', verifyToken, generateFullReportCard);
+router.put('/results/:studentId/report', verifyToken, updateReportCard);
+router.get('/results/class-report', verifyToken, generateClassReport);
+router.get('/performance', verifyToken, getPerformanceAnalytics);
 
 module.exports = router;

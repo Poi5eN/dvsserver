ddc034705ff558ee5bc59d4fe903ea86534ef601
Poi5eN <gaurav.upadhyay.vasudeva@gmail.com>
2025-04-09 15:00:46 +0530

    uuid fix

--- models/receiptModel.js
+++ models/receiptModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
 const receiptSchema = new mongoose.Schema({
   receiptId: { type: String, required: true, unique: true },

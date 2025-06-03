5115e55ad8c55e9c0d6ceb7b7562d202855f0363
Poi5eN <gaurav.upadhyay.vasudeva@gmail.com>
2025-06-03 19:41:10 +0530

    Add CI/CD pipeline

--- migrateSlugs.js
+++ migrateSlugs.js
@@ -12,7 +12,7 @@ const generateSlug = (schoolName) => {
 
 const migrateSlugs = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
